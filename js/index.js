d4b8cb6eed0a4d056d845eda85f55ae0e331f1cb
lukanthrope <bydlocoder228@gmail.com>
2017-12-15 16:51:07 +0300

    m

--- js/index.js
+++ js/index.js
@@ -13,6 +13,16 @@ $(document).ready(function() {
   var country = document.getElementById('country');
   var t = document.getElementById('temp');
   var gradus = document.getElementById('gradus');
+  var ids = ['#temp1', '#temp2', '#temp3', '#temp4', '#temp5', '#temp6'];
+  var lats = ['50.434341', '40.730610', '52.520008', '52.237049', '48.864716', '51.509865'];
+  var longs = ['30.527756', '-73.935242', '13.404954', '21.017532', '2.349014', '-0.118092'];
+  
+  for(var i = 1; i < 7; i++) { 
+        (function(i){$.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lats[i-1] + "&lon=" + longs[i-1], function(dat) {
+          $('#temp' + i).append(dat.main.temp + " &#176" + "C " + dat.weather[0].main + '<img src="' + dat.weather[0].icon + '">');
+          console.log(i);
+        })})(i);
+  }     
   
   if(navigator.geolocation) {
     function success(position) {
@@ -24,7 +34,7 @@ $(document).ready(function() {
         city.innerHTML = data.name + ", ";
         country.innerHTML = data.sys.country;
         
-        t.innerHTML =data.main.temp + " &#176";
+        t.innerHTML = data.main.temp + " &#176";
         gradus.innerHTML = "C ";
         
         $('#gradus').on('click', function(c) {
@@ -43,7 +53,7 @@ $(document).ready(function() {
     };
 
     function error() {
-      out.innerHTML = "Unable to retrieve your location";
+      city.innerHTML = "Unable to retrieve your location";
     };
     navigator.geolocation.getCurrentPosition(success, error);
     
